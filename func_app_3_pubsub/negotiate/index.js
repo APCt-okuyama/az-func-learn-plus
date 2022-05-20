b135ec68c24a98992782abfcabf1dbf7c16985fe
okuyama <t_okuyama@ap-com.co.jp>
2022-05-20 17:37:07 +0900

    update app 3 pubsub document

--- func_app_3_pubsub/negotiate/index.js
+++ func_app_3_pubsub/negotiate/index.js
@@ -1,5 +1,18 @@
 module.exports = function (context, req, connection) {
-    context.log('negotiate start... connection:' + connection);
+    context.log('negotiate start... connection:' + JSON.stringify(connection));
+
+    //connectionにはwebpubsubへの接続に必要な情報(url,access_tokenなど)が含まれる
+    /**
+     * functions.jsonの設定
+        {
+            "type": "webPubSubConnection",
+            "name": "connection",
+            "hub": "simplechat",
+            "userId": "{headers.x-ms-client-principal-name}",
+            "direction": "in"
+        }
+     */
+
     context.res = { body: connection };
     context.done();
 };
