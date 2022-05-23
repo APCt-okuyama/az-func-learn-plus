0a53854297640521b5574dbc2f4bab37cdd5874c
okuyama <t_okuyama@ap-com.co.jp>
2022-05-23 17:15:51 +0900

    cicd test2

--- func_app_6_cicd/HttpTrigger/index.js
+++ func_app_6_cicd/HttpTrigger/index.js
@@ -3,6 +3,6 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "func app 6 cicd is working (updated)"
+        body: "func app 6 cicd is working (updated2)"
     };
 }
